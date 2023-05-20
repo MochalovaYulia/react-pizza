a6da483ad84118838a6304b3b04b6984042edfa4
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-05-20 22:14:25 +0300

    rework debounce

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import styles from './Search.module.scss'
-import {BsSearch} from 'react-icons/bs'
-import {AiOutlineClose} from 'react-icons/ai'
+import { BsSearch } from 'react-icons/bs'
+import { AiOutlineClose } from 'react-icons/ai'
 import { SearchContext } from '../../App'
 import { useState } from 'react'
 import { useRef } from 'react'
@@ -9,35 +9,39 @@ import { useCallback } from 'react'
 import debounce from 'lodash.debounce'
 
 export const Search = () => {
+  const inputRef = useRef();
   const [value, setValue] = useState('');
-  const {setSearchValue} = useContext(SearchContext);
+  const { setSearchValue } = useContext(SearchContext);
 
   const onClickClear = () => {
     setSearchValue('')
     setValue('')
+    inputRef.current.focus();
   }
 
+  // сохранили ссылку на функцию и сделали ее отложенной, вызываем каждый раз когда меняется input
   const updateSearchValue = useCallback(
-    debounce((str) => {
-      setSearchValue(str)
-    }, 500),
+    debounce((value) => {
+      setSearchValue(value)
+    }, 1000),
     []
   )
 
-  const onChangeInput = (event) => {
+  const onChangeInput = event => {
     setValue(event.target.value)
     updateSearchValue(event.target.value)
   }
 
-  return (  
+
+  return (
     <div className={styles.root}>
       <BsSearch className={styles.icon} />
-      <input 
-        autoFocus
-        value={value} 
-        onChange={onChangeInput} 
-        className={styles.input} 
-        placeholder='Поиск пиццы...' 
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
+        className={styles.input}
+        placeholder='Поиск пиццы...'
       />
       {
         value && <AiOutlineClose onClick={onClickClear} className={styles.clearIcon} />
